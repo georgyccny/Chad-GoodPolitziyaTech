9c54bc17418d41042e59f71c7ff7901077cba71f
Imtiaz <imtiazc56@gmail.com>
2023-05-11 14:11:03 -0400

    First

---
+++ djangoweb/homepage/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'homepage/home.html', context)
+
+def about(request):
+    return render(request, 'homepage/about.html', {'title': 'About'})
+
