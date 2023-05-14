ea80eccbaa67fb81a3447847eb2847edd0e6f646
Imtiaz <imtiazc56@gmail.com>
2023-05-14 15:52:30 -0400

    did it. now to scrap it and make it.

--- djangoweb/homepage/urls.py
+++ djangoweb/homepage/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views 
 
 
 urlpatterns = [
-    path('', views.home, name='homepage-home'),
+    path('', PostListView.as_view(), name='homepage-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='homepage-about'),
 ]
+
+# <app>/<model>_<viewtype>.html
